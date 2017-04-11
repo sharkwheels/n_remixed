535a646661a749c0b373ac9e2614b6f2eb9e5172
Nadine Lessio <idoru00@gmail.com>
2017-04-11 14:26:44 -0400

    TABS

--- bot.py
+++ bot.py
@@ -52,20 +52,20 @@ def flickrConnect():
 	# Only do this if we don't have a valid token already
 	if not flickr.token_valid(perms='read'):
 
-    	# Get a request token
-    	flickr.get_request_token(oauth_callback='oob')
+		# Get a request token
+		flickr.get_request_token(oauth_callback='oob')
 
-    	# Open a browser at the authentication URL. Do this however
-    	# you want, as long as the user visits that URL.
-    	authorize_url = flickr.auth_url(perms='read')
-    	webbrowser.open_new_tab(authorize_url)
+		# Open a browser at the authentication URL. Do this however
+		# you want, as long as the user visits that URL.
+		authorize_url = flickr.auth_url(perms='read')
+		webbrowser.open_new_tab(authorize_url)
 
-    	# Get the verifier code from the user. Do this however you
-    	# want, as long as the user gives the application the code.
-    	verifier = str(input('Verifier code: '))
+		# Get the verifier code from the user. Do this however you
+		# want, as long as the user gives the application the code.
+		verifier = str(input('Verifier code: '))
 
-    	# Trade the request token for an access token
-    	flickr.get_access_token(verifier)
+		# Trade the request token for an access token
+		flickr.get_access_token(verifier)
 	return flickr
 
 def getSourceTweets(twitter,max_id,user_from):
